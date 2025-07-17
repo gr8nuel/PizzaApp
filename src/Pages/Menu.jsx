e758b3534c608903b4aff9a2d0b035c1cde9e6da
gr8nuel <139755153+gr8nuel@users.noreply.github.com>
2025-07-17 11:55:16 +0100

    Fix my path to Menu.css

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { MenuList } from "../Helpers/MenuList";
 import MenuItem from "../Components/MenuItem";
-import "../Styles/menu.css";
+import "../Styles/Menu.css";
+
 function Menu() {
   return (
     <div className="menu">
