7ffa4a1bdd1ead6749efd2c3d08b271fa203b98b
gr8nuel <139755153+gr8nuel@users.noreply.github.com>
2025-07-17 16:54:02 +0100

    edit

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -5,19 +5,21 @@ import "../Styles/Menu.css";
 
 function Menu() {
   return (
-    <div className="menu">
-      <h1 className="menuTitle">Our Menu</h1>
-      <div className="menuList">
-        {MenuList.map((menuItem, index) => {
-          return (
-            <MenuItem
-              key={index}
-              image={menuItem.image}
-              name={menuItem.name}
-              price={menuItem.price}
-            />
-          );
-        })}
+    <div className="container">
+      <div className="menu">
+        <h1 className="menuTitle">Our Menu</h1>
+        <div className="menuList">
+          {MenuList.map((menuItem, index) => {
+            return (
+              <MenuItem
+                key={index}
+                image={menuItem.image}
+                name={menuItem.name}
+                price={menuItem.price}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
